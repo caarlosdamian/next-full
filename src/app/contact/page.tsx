32ce01c4d7467aefc6fede7020184efeee725e7d
caarlosdamian <dapecarlos@gmail.com>
2023-06-04 14:20:44 -0600

    feat(components): added contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,48 @@
-
+import Image from 'next/image';
+import styles from './Contact.module.css';
+import { Button } from '@/components';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt="contact"
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            id=""
+            className={styles.input}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            id=""
+            className={styles.input}
+          />
+          <textarea
+            name="message"
+            placeholder="message"
+            id=""
+            cols="30"
+            rows="10"
+            className={styles.textArea}
+          />
+          <Button url="#" text="Send" />
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
