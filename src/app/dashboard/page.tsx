40c7faa634a885acf9633e2df2ef56583757fe7c
caarlosdamian <dapecarlos@gmail.com>
2023-06-11 14:33:43 -0600

    feat(app): finished application

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,54 +1,129 @@
 'use client';
+import React from 'react';
+import styles from './Dashboard.module.css';
+import useSWR from 'swr';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import useSWR from 'swr';
+import Image from 'next/image';
 
 const Dashboard = () => {
-  // const [data, setdata] = useState([]);
-  // const [error, setError] = useState(false);
-  // const [loading, setLoading] = useState(false);
+  //OLD WAY TO FETCH DATA
+
+  // const [data, setData] = useState([]);
+  // const [err, setErr] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
 
   // useEffect(() => {
   //   const getData = async () => {
-  //     setLoading(true);
-  //     setError(false);
-  //     const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
-  //       cache: 'no-store',
+  //     setIsLoading(true);
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //       cache: "no-store",
   //     });
 
   //     if (!res.ok) {
-  //       setError(true);
-  //       setLoading(false);
+  //       setErr(true);
   //     }
+
   //     const data = await res.json()
-  //     setLoading(false);
-  //     setdata(data);
+
+  //     setData(data);
+  //     setIsLoading(false);
   //   };
-  //   getData();
+  //   getData()
   // }, []);
-  // console.log('====testing---',data)
+
   const session = useSession();
+
   const router = useRouter();
+
+  //NEW WAY TO FETCH DATA
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-  const { data, error, isLoading } = useSWR(
-    'https://jsonplaceholder.typicode.com/posts',
+  const { data, mutate, error, isLoading } = useSWR(
+    `/api/posts?username=${session?.data?.user.name}`,
     fetcher
   );
 
   if (session.status === 'loading') {
-    return <p>Loading....</p>;
+    return <p>Loading...</p>;
   }
+
   if (session.status === 'unauthenticated') {
     router?.push('/dashboard/login');
   }
 
-  if (error) return <div>failed to load</div>;
-  if (isLoading) return <div>loading...</div>;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const desc = e.target[1].value;
+    const img = e.target[2].value;
+    const content = e.target[3].value;
 
-  // render data
+    try {
+      await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+          title,
+          desc,
+          img,
+          content,
+          username: session.data.user.name,
+        }),
+      });
+      mutate();
+      e.target.reset();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+      });
+      mutate();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   if (session.status === 'authenticated') {
-    return <div>{JSON.stringify(data)}</div>;
+    return (
+      <div className={styles.container}>
+        <div className={styles.posts}>
+          {isLoading
+            ? 'loading'
+            : data?.map((post) => (
+                <div className={styles.post} key={post._id}>
+                  <div className={styles.imgContainer}>
+                    <Image src={post.img} alt="" width={200} height={100} />
+                  </div>
+                  <h2 className={styles.postTitle}>{post.title}</h2>
+                  <span
+                    className={styles.delete}
+                    onClick={() => handleDelete(post._id)}
+                  >
+                    X
+                  </span>
+                </div>
+              ))}
+        </div>
+        <form className={styles.new} onSubmit={handleSubmit}>
+          <h1>Add New Post</h1>
+          <input type="text" placeholder="Title" className={styles.input} />
+          <input type="text" placeholder="Desc" className={styles.input} />
+          <input type="text" placeholder="Image" className={styles.input} />
+          <textarea
+            placeholder="Content"
+            className={styles.textArea}
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button className={styles.button}>Send</button>
+        </form>
+      </div>
+    );
   }
 };
 
